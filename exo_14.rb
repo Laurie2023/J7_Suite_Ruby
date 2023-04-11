501d43e1f6b7632a59e80cfb592c073413f4ec91
Laurie2023 <129828541+Laurie2023@users.noreply.github.com>
2023-04-11 16:50:35 +0200

    New exo_14

--- exo_14.rb
+++ exo_14.rb
@@ -1,15 +1,18 @@
+emails_list = [] #création d'un tableau avec les adresses emails
+total_email = 0
+email_number=2 #ici car seulement les pairs
+
 puts "Choisis le nombre d'adresses que tu veux :"
 print "> "
 user_number = gets.chomp.to_i
 
-emails_list = Array.new(5) #création d'un tableau avec les adresses emails
-user_number.times do |i| 
-  if (i%2)==0 #si égal à 0 pair, sinon impaire (égal à 1)
-    if i< 10 #création d'un if pour mettre un "0" devant si nombre inférieur à 10
-      emails_list [i]="jean.dupont.0#{i}@email.fr"
-    else
-    emails_list [i]="jean.dupont.#{i}@email.fr"
-    end 
+while total_email < user_number
+  if total_email< 4 #création d'un if pour mettre un "0" devant si nombre inférieur à 10
+     emails_list.push("jean.dupont.0#{email_number}@email.fr")
+  else
+  emails_list.push("jean.dupont.#{email_number}@email.fr")
   end 
+  email_number = email_number+2
+  total_email=total_email+1
 end
 puts emails_list
